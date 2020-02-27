b0d261b93a3638524633413b16984c0664241a61
tominaga <tominaga.switch@gmail.com>
2020-02-27 17:51:05 +0900

    fix item detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qiita_client/models/item.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -45,16 +46,51 @@ class MyHomePage extends StatelessWidget {
           ),
           Expanded(
             child: ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: itemModel.items.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
+              padding: const EdgeInsets.all(8),
+              itemCount: itemModel.items.length,
+              itemBuilder: (BuildContext context, int index) {
+                final item = itemModel.items[index];
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ItemDetail(title: item.title, url: item.url),
+                      ),
+                    );
+                  },
+                  child: Container(
                     height: 50,
-                    child: Text('Entry ${itemModel.items[index].title}'),
-                  );
-                }),
+                    child: Text('Entry ${item.title}'),
+                  ),
+                );
+              },
+            ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class ItemDetail extends StatelessWidget {
+  final String title;
+  final String url;
+
+  const ItemDetail({
+    Key key,
+    this.title,
+    this.url,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: WebView(
+        initialUrl: url,
       ),
     );
   }
